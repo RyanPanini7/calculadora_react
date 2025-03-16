4c9d95c7839397f18773933d2d29f57a20855be8
ryanpanini7 <ryanmpanini@gmail.com>
2025-03-16 11:18:35 -0300

    acrescentado funçao ao display

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,7 +4,17 @@ import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
+
 export default class Calculator extends Component {
+
+  state = {...initialState}
 
   constructor(props) {
     super(props)
@@ -15,7 +25,7 @@ export default class Calculator extends Component {
   }
       
   clearMemory(){
-    console.log('limpar')
+    this.setState({ ...initialState})
   }
 
   setOperation(operation) {
@@ -23,13 +33,21 @@ export default class Calculator extends Component {
   }
 
 addDigit(n) {
-  console.log(n)
+  if (n === '.' && this.state.displayValue.includes('.')) {
+    return
+  }
+
+  const clearDisplay = this.state.displayValue === '0'
+   || this.state.clearDisplay
+  const currentValue = clearDisplay ? '' : this.state.displayValue
+  const displayValue = currentValue + n
+  this.setState({ displayValue, clearDisplay: false})
 }
 
   render (){
     return(
       <div className="calculator">
-      <Display value={100}/>
+      <Display value={this.state.displayValue}/>
       <Button label="AC" click={this.clearMemory} triple />
       <Button label="/" click={this.setOperation} operation/>
       <Button label="7" click={this.addDigit}/>
@@ -45,7 +63,7 @@ addDigit(n) {
       <Button label="3" click={this.addDigit}/>
       <Button label="+" click={this.setOperation} operation/>
       <Button label="0" className="zero" click={this.addDigit} double />
-      <Button label="." click={this.setOperation} operation/>
+      <Button label="." click={this.addDigit} />
       <Button label="=" click={this.setOperation} operation/>
     </div>
     )
